aa740c48771ac2fb561f83b206aa3c9e40dc621d
Mario Sandoval Velázquez <mario.sandoval@fonyou.com>
2025-11-23 01:00:51 -0600

    Styles. Se establece configuración global de estilos del proyecto.
    -Se crean estilos globales
    - Se establecen Variables de estilos
    - Se crean clases de color personalizadas de tailwimd
    - Se configura Layout para usar la fuente Jakarta Sans como fuente principañ del proyecto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-plus-jakarta",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={plusJakarta.variable}
       >
         {children}
       </body>
