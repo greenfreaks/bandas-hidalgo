73c3e81e8436577896baf55c7e5df40ac05f9eb7
Mario Sandoval Velázquez <mario.sandoval@fonyou.com>
2026-05-23 01:21:56 -0600

    Add. Todos los componentes de la página de la banda, se meten en un sólo componente

--- src/app/(main)/banda/[idBanda]/page.tsx
+++ src/app/(main)/banda/[idBanda]/page.tsx
@@ -1,10 +1,6 @@
 import { bandCatalog } from "@/mocks/bandas";
 import { notFound } from "next/navigation";
-import { BannerBand } from "./components/banner-band";
-import { SpotifyTracksBand } from "./components/spotify-tracks-band";
-import { BandEvents } from "./components/band-events";
-import { BandMembers } from "./components/band-members";
-import { BandVideos } from "./components/band-videos";
+import { Band } from "./components/band";
 
 interface SlugBandProps {
   params: Promise<{idBanda: string}>
@@ -23,21 +19,6 @@ export default async function SlugBand({ params }: SlugBandProps) {
     return notFound();
   }
   return (
-    <section>
-      <BannerBand banda={banda} />
-      {banda.tracksOnSpotify && (
-        <SpotifyTracksBand bandaTracks={banda.tracksOnSpotify} />
-      )}
-      {banda.events && (
-        <BandEvents eventsBand={banda.events} mainColorBand={banda.mainColor} />
-      )}
-      {banda.members && (
-        <BandMembers
-          bandMembers={banda.members}
-          mainColorBand={banda.mainColor}
-        />
-      )}
-      {banda.videos && <BandVideos videos={banda.videos} />}
-    </section>
+    <Band banda={banda} /> 
   );
 }
